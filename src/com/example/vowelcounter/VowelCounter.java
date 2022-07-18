c4a739b42cb60a2d8927a6d6aa34f43a1906d683
Marcus Davis <davismarcus0102@gmail.com>
2022-07-17 20:59:35 -0400

    Added new features: character count and word count

--- src/com/example/vowelcounter/VowelCounter.java
+++ src/com/example/vowelcounter/VowelCounter.java
@@ -1,13 +1,18 @@
 package com.example.vowelcounter;
 
+import javax.swing.*;
+import java.awt.*;
 import java.util.Arrays;
-import java.util.Scanner;  // Import the Scanner class
+
 
 // this application counts the number of vowels in a word/sentence the user inputs
 public class VowelCounter {
     String[] vowels = {"a", "e", "i", "o", "u"};
+    String[] consonants = {"b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"};
     String conditionalVowel = "y";
     int numVowels = 0;
+    int numCharacters = 0;
+    int numWords = 0;
 
     public VowelCounter(){
 
@@ -37,6 +42,7 @@ public class VowelCounter {
         return conditionalVowel;
     }
 
+
     // takes a string (user input) - counts the number of vowels
     // returns an int
     public int getNumVowels(String userInput) {
@@ -50,17 +56,75 @@ public class VowelCounter {
         return numVowels;
     }
 
+
+    // takes a string (user input) - counts the number of characters
+    // returns an int
+    public int getNumCharacters(String userInput) {
+        // if user input is empty, request again, give option to cancel
+        if (userInput.isBlank()) {
+            JOptionPane.showMessageDialog(null, "You didn't enter anything", "error", JOptionPane.ERROR_MESSAGE);
+            JOptionPane.showInputDialog(null, "Enter text here: ", "character counter", JOptionPane.OK_CANCEL_OPTION);
+            //if (JOptionPane.CANCEL_OPTION) System.exit(0);
+        }
+        for (int i = 0; i < userInput.length(); i++){
+            numCharacters++;
+             if (userInput.charAt(i) == ' ') {
+                 numCharacters--;
+             }
+        }
+        return numCharacters;
+    }
+
+    // takes a string (user input) - counts the number of words
+    // returns an int
+    public int getNumWords(String userInput) {
+        // use split() method to separate words and store them in an array
+        String[] arry = userInput.split(" ");
+        numWords = arry.length;
+
+        return numWords;
+    }
+
+
     public static void main(String[] args) {
+        int count = 0;
+        String userInput;
         VowelCounter readWord = new VowelCounter();
-        int vowelCount = 0;
 
-        Scanner scanner = new Scanner(System.in);  // Create a Scanner object
-        System.out.println("Vowel Counter \nEnter your word here: ");
+        // options for drop down menu on first page
+        String[] options = {"Count vowels", "Count characters", "Count words"};
 
-        String userInput = scanner.nextLine();  // Read user input
-        vowelCount = readWord.getNumVowels(userInput);
+        // welcome screen image
+        JLabel labelN = new JLabel();
+        JLabel labelS = new JLabel();
+        JLabel labelSh = new JLabel();
+        JLabel labelIU = new JLabel();
+        labelN.setIcon(new ImageIcon(new ImageIcon("naruto-uzumaki.png").getImage().getScaledInstance(300, 200, Image.SCALE_DEFAULT)));
+        labelS.setIcon(new ImageIcon(new ImageIcon("sasuke.jpeg").getImage().getScaledInstance(300, 200, Image.SCALE_DEFAULT)));
+        labelSh.setIcon(new ImageIcon(new ImageIcon("shikamaru.jpeg").getImage().getScaledInstance(300, 200, Image.SCALE_DEFAULT)));
+        labelIU.setIcon(new ImageIcon(new ImageIcon("itachi-Uchiha.jpeg").getImage().getScaledInstance(300, 200, Image.SCALE_DEFAULT)));
 
-        // Output
-        System.out.println(userInput + " has " + vowelCount + " vowels");
+        // first page - gets what action the user wants to do
+        String userSelection = (String)JOptionPane.showInputDialog(null, "What would you like to do? ", "hi", JOptionPane.QUESTION_MESSAGE, labelN.getIcon(), options, options[0]);
+
+        if (userSelection.equals("Count vowels")){
+            userInput = JOptionPane.showInputDialog(null, "Enter text here: ", "Vowel Counter", JOptionPane.PLAIN_MESSAGE);
+            count = readWord.getNumVowels(userInput);
+            JOptionPane.showMessageDialog(null, "Vowel count: " + count, "Vowel Counter", JOptionPane.PLAIN_MESSAGE, labelS.getIcon());
+        } else if (userSelection.equals("Count characters")) {
+            userInput = JOptionPane.showInputDialog(null, "Enter text here: ", "Character Counter", JOptionPane.PLAIN_MESSAGE);
+            count = readWord.getNumCharacters(userInput);
+            JOptionPane.showMessageDialog(null, "Character count: " + count, "Character Counter", JOptionPane.PLAIN_MESSAGE, labelSh.getIcon());
+        } else if (userSelection.equals("Count words")) {
+            userInput = JOptionPane.showInputDialog(null, "Enter text here: ", "Word Counter", JOptionPane.PLAIN_MESSAGE);
+            count = readWord.getNumWords(userInput);
+            JOptionPane.showMessageDialog(null, "Word count: " + count, "Word counter", JOptionPane.PLAIN_MESSAGE, labelIU.getIcon());
+        }
+
     }
 }
+
+// exception catching
+// might spell check
+// add y , read file?
+// clean up code
